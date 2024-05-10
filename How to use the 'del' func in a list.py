5bf0542cd6307cfc81d823288be377455198ea42
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-05-10 10:04:40 +0100

    Adding New

---
+++ How to use the 'del' func in a list.py
@@ -0,0 +1,8 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]# here is the list
+# first i will delete the the nummber for the list using |INDEX| number
+del list1[3]#3 is the index number not the element
+# after this you will see the change in the list
+print(list1)
+#here again I am want to delete the whole list this is how i gonna use the del function in a list
+del list1[:]
+print(list1)#here the list got empty [] in the output
