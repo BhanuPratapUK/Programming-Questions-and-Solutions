75b1b38e3bb92d6c27c71cb51527c85d460bfa81
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-05-11 21:26:49 +0100

    Adding New

---
+++ Use of IF-ELIF Nested Methods.py
@@ -0,0 +1,7 @@
+Text= input('Here is the ASCII-->')
+if Text.isdigit():
+    print('This Text is One of the DIGITS')
+elif Text.isalpha():
+    print('This Text is One of the Alphabatical')
+elif Text.isspace():## Here give a space then press enter otherwise its not gonna show Anything...
+    print('This Text is WHITESPACES')  
