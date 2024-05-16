ecba4fa0004ee599f11c1dab39ae154ab5ef63c6
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-05-16 09:56:34 +0100

    Adding New

---
+++ program to illustrate updating a set and concepts.py
@@ -0,0 +1,10 @@
+set1= {21,22,23,24}
+print(set1[0])
+'''
+    print(set1[0])
+          ~~~~^^^
+TypeError: 'set' object is not subscriptable
+
+it will show this error becoz set donot allow programmers to change or accces the elements using indicing
+
+'''
