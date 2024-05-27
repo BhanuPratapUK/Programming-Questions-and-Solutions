71810121f37c9810fa638414e09455bc73ce8b72
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-05-27 13:36:20 +0100

    Adding New

---
+++ program of left ,right and center judtifiers.py
@@ -0,0 +1,4 @@
+A = 'Coding'
+print('Here is the new format left -->',format(A,'<10'))
+print('Here is the new format right -->',format(A,'>10'))
+print('Here is the Center Aligns-->',format(A,'^10'))
