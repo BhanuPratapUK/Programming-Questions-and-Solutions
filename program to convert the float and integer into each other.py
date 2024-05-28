1384e18c13942a19b9e842c631ef07a920b2e5a1
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-05-28 10:28:24 +0100

    Adding New

---
+++ program to convert the float and integer into each other.py
@@ -0,0 +1,7 @@
+Float_number = float(input('Enter yhe number is--->'))
+print('CONVERTING into INTEGER->',int(Float_number))
+
+print('****************************************************')
+
+integer = int(input('Enter the number is --->>'))
+print('CONVERTING into FLOATING NUMBER-->',float(integer))
