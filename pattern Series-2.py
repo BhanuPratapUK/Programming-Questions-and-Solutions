db8f76274d324876b4db79cdfd90ddc72462bc8f
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-03 15:10:56 +0100

    Adding New

---
+++ pattern Series-2.py
@@ -0,0 +1,18 @@
+print('PATTERN SERIES')
+
+for i  in range(1,6):
+    print('Pass',i,'-',end='  ')
+    for j in range(1,6):
+        print(j,end='  ')
+    print()
+
+'''
+OUTPUT
+
+Pass 1 -  1  2  3  4  5  
+Pass 2 -  1  2  3  4  5
+Pass 3 -  1  2  3  4  5
+Pass 4 -  1  2  3  4  5
+Pass 5 -  1  2  3  4  5
+
+'''
