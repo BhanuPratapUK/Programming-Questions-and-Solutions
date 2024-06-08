37be6e299cdb11e51e2e3097c491e891b30f32b6
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-08 16:00:33 +0100

    Adding New

---
+++ Program to  know the lenth of any list, string, tuple,dict of  elemens.py
@@ -0,0 +1,9 @@
+msg = 'hey'
+list1 = [11,12,22,33]
+tuple1= (1,2,3,4,6)
+dict1 = {1:'hy',2:'yo'}
+'''By usig len method'''
+print('The length of the string--->',len(msg))
+print('The length of the List--->',len(list1))
+print('The length of the tuple1-->',len(tuple1))
+print('The length of the list1-->',len(dict1))
