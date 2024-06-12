660ff1894f1efc5d65db391c1d2e05c84a1f0868
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-12 10:56:44 +0100

    Adding New

---
+++ Progroam to know he all and any operations of a list.py
@@ -0,0 +1,8 @@
+a = [1,2,3,4,0]
+print(all(a))
+print(any(a))
+'''It will return the value in a boolean and the output will be [False] if there is a ZERO element 
+present  and if we apply ANY operations in a list it will give the [TRUE] the list 
+shouldn't be empty in both cases other wise it gonna return the output wll  be False
+
+'''
