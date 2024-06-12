175457ec5a6cdb78583afd3afcd78686d0adf6ed
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-12 11:26:48 +0100

    Adding New

--- Progroam to know he all and any operations of a list.py
+++ Progroam to know he all and any operations of a list.py
@@ -4,5 +4,4 @@ print(any(a))
 '''It will return the value in a boolean and the output will be [False] if there is a ZERO element 
 present  and if we apply ANY operations in a list it will give the [TRUE] the list 
 shouldn't be empty in both cases other wise it gonna return the output wll  be False
-
 '''
---
+++ program to sort the list.py
@@ -0,0 +1,3 @@
+list1= [110,1,2,3,4,12,13,24,545]
+list1.sort()
+print('The SORted New list is here --->>',list1)
