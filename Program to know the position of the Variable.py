80fccfce1dec98941e03cb5ed38936349aea2425
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-15 12:29:50 +0100

    ADDING NEw

---
+++ Program to know the position of the Variable.py
@@ -0,0 +1,5 @@
+List1= [19,61,12,13,14,15]
+print('The 15 is present at which position in a list of a string ->',List1.index(15))
+''' There are several types of BuIlt_in_Methods  and INDEX is one of them 
+through which we can know the index at which it  is present in a list 
+'''
