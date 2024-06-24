06e5df25658f370d21249b9f4574d408ce58bde5
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-24 23:58:56 +0100

    Adding New

--- Program to form the first charracter odf every word in another list.py
+++ Program to form the first charracter odf every word in another list.py
@@ -7,7 +7,7 @@ print('The fist charcter of the list1-->>',New_list)
 
 print('**************************************************')
 # we can do by iterate also 
-list1 = ['Boy','hat','apple','nail','umbrella']
+list1 = ['Boy','htot','apple','nail','umbrella']
 New_list  =[]
 for i in range(len(list1)):
     New_list.append(list1[i][0])
