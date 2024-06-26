1b40d5be455ab17ad11e300681b96e3e671de280
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-26 13:29:24 +0100

    Adding New

---
+++ Concept of Dictionary.py
@@ -0,0 +1,15 @@
+'''1-  A Dictionary is a one of the part of data structure and represented as a 
+       pair in form of "KEY" and its "VALUE"
+   2-  Dictionary is stored in Curly Brackets {}
+   3-  To create an Empty dictionary the syntax we use {} for its key and its value
+       EXAMPLE- {1:Akash,1:Amit}
+   4-  To create an Empty dictionary the syntax is []
+       hashmap[nums[i]]=i --> here it is storing key and value while iterating one by
+       one through loop
+   5-  Every key Will be separated from its value using COLON(:) in a dictionary
+   6-  Also, we can convert the sequence of key value into a dictionary
+       for example->dict([(1,Akash),(2,Amit)])
+   
+   '''
+Text= dict([(1,'Akash'),(2,'Amit')])
+print('Dictionary in the form of key and its values->',Text)
