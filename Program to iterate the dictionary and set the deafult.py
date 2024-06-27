d9811a7335a9c9fd2149e273d114eb11c8321742
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-27 01:43:47 +0100

    Adding New

---
+++ Program to iterate the dictionary and set the deafult.py
@@ -0,0 +1,8 @@
+Dict1={1:'Bhanu',2:'Amit',3:'Akash','class':'ooops'}
+Dict1['piyush']= 'kapoor'
+Dict1.setdefault('marks',1000)
+for i in Dict1.items():
+    print(i)
+print('The Value of this Key is--->',Dict1.get('piyush'))
+
+
---
+++ Program using string formatting to get the key-value that strored in dictionary.py
@@ -0,0 +1,3 @@
+dict1= {'Bhanu':1,'Akash':2,'Amit':3}
+for key,val in dict1.items():
+    print('%s  roll no is --> %i'%(key,val))
