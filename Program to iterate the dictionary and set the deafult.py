b1beb5e6b2f68c65e4fbe9cc199c47a89ea89fd3
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-27 01:49:06 +0100

    Adding New

--- Program to iterate the dictionary and set the deafult.py
+++ Program to iterate the dictionary and set the deafult.py
@@ -3,6 +3,6 @@ Dict1['piyush']= 'kapoor'
 Dict1.setdefault('marks',1000)
 for i in Dict1.items():
     print(i)
-print('The Value of this Key is--->',Dict1.get('piyush'))
+print('The Value of this Key  is--->',Dict1.get('piyush'))
 
 
