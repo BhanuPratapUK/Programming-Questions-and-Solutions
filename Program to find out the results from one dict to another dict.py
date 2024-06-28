224ed5d40c77c11bb17d9291938bcd23d8465e7f
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-06-28 10:41:26 +0100

    Adding New

---
+++ Program to find out the results from one dict to another dict.py
@@ -0,0 +1,4 @@
+e_h = {'Book':'Pustak','Friend':'Dost'}
+h_u = {'Pustak':'kitab','Dost': 'mitr'}
+for i in e_h:
+    print(i,'has the meaning in hindi is -->',e_h[i],'-- and its meaning in urdu is -->',h_u[e_h[i]])
