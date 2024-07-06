3d50d79e8459a14f96be5f29d728e32ca6af1fb1
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-07-06 16:26:52 +0100

    Adding

---
+++ Datatype of the input number and using int for it .py
@@ -0,0 +1,8 @@
+x = input('Enter the number is =>')
+y = input('Enter the number is =>')
+print(x+y)
+
+print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+a = int(input('Enter the number is =>'))
+b = int(input('Enter the number is =>'))
+print('Here it is-->',a+b)
--- Format function.py
+++ Format function.py
@@ -1,10 +1,16 @@
-''' 
-Suppose we get a object in float and we want the value upto 2 or 3 decimal so we can use 
-"format"  method there is another method known as round method.
-format(value, format specifier)
-round(format(value, format specifier))
-'''
-x = 10/3
-print('Here is the answer without using any method-->',x)
-y = format(x,'.2f')
-print('Here is the answer using any method--->',y)
+# ''' 
+# Suppose we get a object in float and we want the value upto 2 or 3 decimal so we can use 
+# "format"  method there is another method known as round method.
+# format(value, format specifier)
+# round(format(value, format specifier))
+# '''
+# x = 10/3
+# print('Here is the answer without using any method-->',x)
+# y = format(x,'.2f')
+# print('Here is the answer using any method--->',y)
+
+alert = "This is an alert sound\a"
+form_feed = "First Page\fSecond Page"
+
+
+print(form_feed)
