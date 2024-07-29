4c36d77d1c35045a7913c51ece8930ccecfbc4c0
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-07-29 01:00:40 +0100

    Adding

---
+++ Program to Transversal the string.py
@@ -0,0 +1,9 @@
+text = input("Enter the message ----> ")
+index= 0
+for x in text:
+    print('Text-',index,'-->',x)
+    index+=1
+    '''
+Transversal meaning Iterating the characters from one place to another
+    
+    '''
