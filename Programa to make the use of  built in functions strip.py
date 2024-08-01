50db7a50dcc5c5a910a9abd5bb166bc1d34097b6
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-08-01 23:50:07 +0100

    Adding

---
+++ Programa to make the use of  built in functions strip.py
@@ -0,0 +1,4 @@
+msg="  Hi Pyhton how are you   "
+print('All left whiespaces remove from the string-->',msg.lstrip())
+print('All the right whitespaces remove  from the string-->',msg.rstrip())
+print('All strip remove from the string from left and right-->',msg.strip())
