c98d0e170ddb9aee864664bb3a607870a5095742
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-08-02 22:38:09 +0100

    Adding

---
+++ Program to use the list remove methods.py
@@ -0,0 +1,6 @@
+list1= [1,2,3,4,5,6,7,'F']
+print('The original list->',list1)
+list1.remove(4)
+print('The New list after remove the 4 from the original list-->',list1)
+'''In remove() it requires the value not the index 
+which has to be removed from the list1->'''
