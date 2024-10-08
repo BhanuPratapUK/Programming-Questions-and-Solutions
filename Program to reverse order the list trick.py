9976b92c0277a52497e6b3a5efe4072932e5633f
Bhanu Pratap <ppratapbhanu0@gmail.com>
2024-10-08 16:10:48 +0100

    Adding

---
+++ Program to reverse order the list trick.py
@@ -0,0 +1,6 @@
+list1 = [2,34,5,7,8,9]
+list1.reverse()
+print('Here is the reverse order of the list-->',list1)
+list2 =['a','3','bhb',90]
+x = list2[::-1]
+print('Here is the reverse order of the list-->',x)
